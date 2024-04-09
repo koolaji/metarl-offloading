bfe37072a82e131572cc12d47efafa5025e3545f
root <root@debian-c-4-8gib-fra1-01>
2024-04-09 22:17:39 +0000

    second update

--- meta_trainer.py
+++ meta_trainer.py
@@ -9,6 +9,12 @@ warnings.filterwarnings("ignore", category=UserWarning, module="gym")
 if __name__ == "__main__":
     from env.mec_offloaing_envs.offloading_env import Resources  # import resource spec
     from env.mec_offloaing_envs.offloading_env import OffloadingEnvironment  # import env spec
+    from policies.meta_seq2seq_policy import  Seq2SeqPolicy
+    from baselines.vf_baseline import ValueFunctionBaseline
+    from policies.meta_seq2seq_policy import MetaSeq2SeqPolicy
+    from samplers.seq2seq_meta_sampler import Seq2SeqMetaSampler
+
+
     META_BATCH_SIZE = 2    
     resource_cluster = Resources(mec_process_capable=(10.0 * 1024 * 1024),
                                  mobile_process_capable=(1.0 * 1024 * 1024),
@@ -39,11 +45,25 @@ if __name__ == "__main__":
                                     # "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_25/random.20.",
                                 ],
                                 time_major=False)
-    logging.debug('start of greedy_solution')
+    logging.info('start of greedy_solution')
     action, greedy_finish_time = env.greedy_solution()
-    logging.debug('end of greedy_solution')
-    logging.debug("avg greedy solution: %s", np.mean(greedy_finish_time))
+    logging.info('end of greedy_solution')
+    logging.info("avg greedy solution: %s", np.mean(greedy_finish_time))
     finish_time = env.get_all_mec_execute_time()
-    logging.debug("avg all remote solution: %s", np.mean(finish_time))
+    logging.info("avg all remote solution: %s", np.mean(finish_time))
     finish_time = env.get_all_locally_execute_time()
-    logging.debug("avg all local solution: %s", np.mean(finish_time))
+    logging.info("avg all local solution: %s", np.mean(finish_time))
+
+    baseline = ValueFunctionBaseline()
+
+    meta_policy = MetaSeq2SeqPolicy(meta_batch_size=META_BATCH_SIZE, obs_dim=17, encoder_units=128, decoder_units=128,
+                                    vocab_size=2)
+
+    # sampler = Seq2SeqMetaSampler(
+    #     env=env,
+    #     policy=meta_policy,
+    #     rollouts_per_meta_task=1,  # This batch_size is confusing
+    #     meta_batch_size=META_BATCH_SIZE,
+    #     max_path_length=20000,
+    #     parallel=False,
+    # )   
