0465f35ba6e3cda4263c59d3b9b6818f1137e90b
root <root@debian-c-4-8gib-fra1-01>
2024-04-14 20:03:06 +0000

    add org folder

--- meta_trainer.py
+++ meta_trainer.py
@@ -9,11 +9,8 @@ warnings.filterwarnings("ignore", category=UserWarning, module="gym")
 if __name__ == "__main__":
     from env.mec_offloaing_envs.offloading_env import Resources  # import resource spec
     from env.mec_offloaing_envs.offloading_env import OffloadingEnvironment  # import env spec
-    from policies.meta_seq2seq_policy import  Seq2SeqPolicy
     from baselines.vf_baseline import ValueFunctionBaseline
     from policies.meta_seq2seq_policy import MetaSeq2SeqPolicy
-    from samplers.seq2seq_meta_sampler import Seq2SeqMetaSampler
-    from samplers.seq2seq_meta_sampler_process import Seq2SeqMetaSamplerProcessor
 
 
     META_BATCH_SIZE = 2    
@@ -26,7 +23,7 @@ if __name__ == "__main__":
                                 graph_number=100,
                                 graph_file_paths=[
                                     "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_1/random.20.",
-                                    "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_2/random.20.",
+                                    # "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_2/random.20.",
                                     # "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_3/random.20.",
                                     # "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_5/random.20.",
                                     # "./env/mec_offloaing_envs/data/meta_offloading_20/offload_random20_6/random.20.",
@@ -60,16 +57,3 @@ if __name__ == "__main__":
     meta_policy = MetaSeq2SeqPolicy(meta_batch_size=META_BATCH_SIZE, obs_dim=17, encoder_units=128, decoder_units=128,
                                     vocab_size=2)
 
-    sampler = Seq2SeqMetaSampler(
-        env=env,
-        policy=meta_policy,
-        rollouts_per_meta_task=1,  # This batch_size is confusing
-        meta_batch_size=META_BATCH_SIZE,
-        max_path_length=20000,
-        parallel=False,
-    )   
-    sample_processor = Seq2SeqMetaSamplerProcessor(baseline=baseline,
-                                                   discount=0.99,
-                                                   gae_lambda=0.95,
-                                                   normalize_adv=True,
-                                                   positive_adv=False)
